587226d6910d26737686766061f6929d185a8b2f
ZhouZ <16379978+ZhouZ-1@users.noreply.github.com>
2022-09-03 18:37:26 +1000

    test floating point precision

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -2,6 +2,11 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
+    let a = 1.1;
+    let b = 2.2;
+    let c = a + b;
+    println!("{} + {} = {}", a, b, c);
+
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
